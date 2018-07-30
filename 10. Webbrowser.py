0f2d9ae8a4fd88b943aecea62dc35a5d95c14aa1
Soumyadip <41679833+soumyadipghosh432@users.noreply.github.com>
2018-07-30 22:36:15 +0530

    Update 10. Webbrowser.py

--- 10. Webbrowser.py
+++ 10. Webbrowser.py
@@ -14,9 +14,11 @@ print(webbrowser._browsers)
 webbrowser.get('google-chrome').open(a_website) # for linux need to mention the browser name
 webbrowser.get('firefox').open(b_website) # for linux need to mention the browser name
 
+print("Browser runs in parallel with this code. this will be executed even if the browser is open")
+
 webbrowser.open("https://www.python.org/") # for windows if the default browser is set
 
-#if in windows if need to register and run in a browser not available directly
-chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\firefox.exe"
+#if browser is not available and need to register to run
+chrome_path="C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe" #path for windows system
 webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
 webbrowser.get('chrome').open_new_tab(b_website)
